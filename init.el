e72247f69d6008e6d289cf4073017ac830622d08
Niankun Rao <Niankun_Rao@selinc.com>
2020-01-22 14:38:19 -0800

    remove irony package

--- init.el
+++ init.el
@@ -80,8 +80,6 @@ values."
       auto-package-update
       atom-one-dark-theme
       function-args
-      irony
-      company-irony
      )
    ;;  list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -429,15 +427,6 @@ before packages are loaded. If you are unsure, you should try in setting them in
 
   ;; enable company mode globaly
   (add-hook 'after-init-hook 'global-company-mode)
-
-  (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))
-  ;; Windows performance tweaks
-  ;;
-  (when (boundp 'w32-pipe-read-delay)
-    (setq w32-pipe-read-delay 0))
-  ;; Set the buffer size to 64K on Windows (from the original 4K)
-  (when (boundp 'w32-pipe-buffer-size)
-    (setq irony-server-w32-pipe-buffer-size (* 64 1024)))
 )
 
 (defun dotspacemacs/user-config ()
